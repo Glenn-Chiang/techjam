097d3c7bd903b1159758cfb2742c70b7d8322d14
Florian Viorel Tanely <izkx73@gmail.com>
2025-08-31 01:57:34 +0800

    Add signup api handler

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,11 +1,8 @@
+import type { UserData } from "../types/types.js";
 import { BASE_URL } from "./baseApi.js";
 
-export interface LoginResponse {
-  token: string;
-}
-
-export const login = async (email: string, password: string): Promise<LoginResponse> => {
-  const response = await fetch(`${BASE_URL}/login`, {
+export const login = async (email: string, password: string): Promise<UserData> => {
+  const response = await fetch(`${BASE_URL}/auth/login`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ email, password }),
@@ -17,6 +14,21 @@ export const login = async (email: string, password: string): Promise<LoginRespo
 
   return response.json();
 };
+
+export const signUp = async (name: string, email: string, password: string): Promise<UserData> => {
+  const response = await fetch(`${BASE_URL}/auth/signup`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ name, email, password }),
+  });
+
+  if (!response.ok) {
+    throw new Error('Signup failed');
+  }
+
+  return response.json();
+};
+
 
 export const setToken = (token: string) => {
   localStorage.setItem('token', token);
